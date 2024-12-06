21f710ecd5810fc10e0ee71979c5cfb02533c85a
radiabajaa <bajaamody@gmail.com>
2024-12-06 11:02:11 +0300

    first commit

---
+++ data/bizinfo/bizinfo.eb4_basic.config.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined('_EYOOM_')) exit;
+$bizinfo = array(
+	"bi_company_name" => "P2U",
+	"bi_company_bizno" => "OOO-OO-OOOOO",
+	"bi_company_ceo" => "P2U",
+	"bi_company_tel" => "212-202-52",
+	"bi_company_fax" => "08502",
+	"bi_company_sellno" => "제2024-서울0326호",
+	"bi_company_bugano" => "",
+	"bi_company_infoman" => "P2U",
+	"bi_company_infomail" => "webmaster@domain.com",
+	"bi_cs_tel1" => "443-86-2041",
+	"bi_cs_tel2" => "",
+	"bi_cs_fax" => "212-202-52",
+	"bi_cs_email" => "p2u@domain.com",
+	"bi_cs_time" => "평일 10:00 ~ 17:00
+토요일 12:00 ~ 13:00",
+	"bi_cs_closed" => "일요일 및 공휴일은 휴무",
+	"bi_company_zip" => "",
+	"bi_company_addr1" => "",
+	"bi_company_addr2" => "",
+	"bi_company_addr3" => "",
+	"bi_company_addr_jibeon" => "",
+	"bi_top_logo" => "be40cd0cd01209b4a21423f85fe24611_PhSX3vpo_c4e75c35a8ddd53ad1391c34d458409dff7a68b2.png",
+);
